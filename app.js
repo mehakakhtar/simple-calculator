4d4623737b10a5af944c3d98a72b6c73b258b670
mehakakhtar <mehakakhtar47@gmail.com>
2025-04-27 19:36:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = parseInt(prompt("Enter the first number:"));
+let b = parseInt(prompt("Enter the second number:"));
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log("Calculator Results:");
+console.log("Sum: " + addition);
+console.log("Difference: " + subtraction);
+console.log("Product: " + multiplication);
+console.log("Quotient: " + division);
+
+
